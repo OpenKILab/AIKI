368df0ea2f9ad2630c08623057d3bd4ada0a69a5
banana <sys555@outlook.com>
2024-10-29 15:12:10 +0800

    edit basereranker rerank func input

--- aiki/retriever/reranker.py
+++ aiki/retriever/reranker.py
@@ -1,30 +1,9 @@
 from typing import List, Dict, Any
+from aiki.modal.retrieval_data import RetrievalData
 
 class BaseReranker:
     def __init__(self, config):
         self.config = config
         
-    def rerank(self, query_list: List[Dict[str, Any]], data_list: List[List[Dict[str, Any]]], topk: int = None) -> List[List[Dict[str, Any]]]:
-        # query_list example:
-        # [
-        #     {
-        #         "text": "What is the capital of France?",
-        #         "image": image_data,  # Optional
-        #         "audio": audio_data   # Optional
-        #     },
-        #     ...
-        # ]
-
-        # data_list example:
-        # [
-        #     [
-        #         {
-        #             "document_id": "doc1",
-        #             "content": "Paris is the capital of France.",
-        #             "score": 0.8
-        #         },
-        #         ...
-        #     ],
-        #     ...
-        # ]
+    def rerank(self, query: RetrievalData, data_list: RetrievalData, topk: int = None) -> RetrievalData:
         ...
