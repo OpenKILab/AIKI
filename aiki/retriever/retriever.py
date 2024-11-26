d0af69553818bb456706ecdedcccb106a264e072
banana <sys555@outlook.com>
2024-11-26 14:50:59 +0800

    clip retrieve

--- aiki/retriever/retriever.py
+++ aiki/retriever/retriever.py
@@ -5,6 +5,7 @@ from typing import List, Dict, Protocol
 from bson import ObjectId
 from aiki.database.chroma import ChromaDB
 from aiki.database.json_file import JSONFileDB
+from aiki.embedding_model.embedding_model import EmbeddingModel, JinnaClip
 from aiki.modal.retrieval_data import KVSchema, RetrievalData, RetrievalType, RetrievalItem
 from aiki.multimodal.base import ModalityType, MultiModalProcessor
 from aiki.multimodal.image import ImageHandlerOP
@@ -44,15 +45,22 @@ class RecallStrategy(Protocol):
         ...
 
 class BaseRetriever(ABC):
-    def __init__(self, processor: MultiModalProcessor, preretriever_components: List[PreRetrieverComponent] = None, postretriever_components: List[PostRetrieverComponent] = None, recall_strategies: List[RecallStrategy] = None):
+    def __init__(
+        self, 
+        processor: MultiModalProcessor, 
+        preretriever_components: List[PreRetrieverComponent] = None, 
+        postretriever_components: List[PostRetrieverComponent] = None, 
+        recall_strategies: List[RecallStrategy] = None, 
+        embedding_model: EmbeddingModel = None
+    ):
         self.topk = 4
         self.recall_strategies = recall_strategies or []
         self.database = {}
         self.data_pool = DataPool()
-        self.preretriever_components = []
-        self.postretriever_components = []
+        self.preretriever_components = preretriever_components or []
+        self.postretriever_components = postretriever_components or []
         self.processor = processor
-        
+        self.embedding_model = embedding_model
     def pre_retrieve(self, query: RetrievalData):
         for component in self.preretriever_components:
             query = component.process(query)
@@ -80,14 +88,25 @@ class BaseRetriever(ABC):
 
     
 class DenseRetriever(BaseRetriever):
-    def __init__(self, processor):
-        super().__init__(processor)
+    def __init__(
+        self, 
+        processor: MultiModalProcessor, 
+        embedding_model: EmbeddingModel=None
+        ):
+        super().__init__(processor, embedding_model)
+        self.processor = processor
+        self.embedding_model = embedding_model
         
     def _search(self, query: RetrievalData, num: int = 10):
         queries = [q.content for q in query.items if q.__class__ == TextModalityData]
         start_time = query.items[0].metadata.get("start_time", 0)
         end_time = query.items[0].metadata.get("end_time", int((datetime.now()).timestamp()))
-        vector_db_result = self.processor.execute_operation(ModalityType.VECTOR, VectorHandlerOP.QUERY, queries, top_k=num, start_time = start_time, end_time = end_time)
+        if self.embedding_model:
+            query_embeddings = self.embedding_model.embed(query)
+            print(query_embeddings)
+            vector_db_result = self.processor.execute_operation(ModalityType.VECTOR, VectorHandlerOP.QUERY, query_embeddings=query_embeddings, top_k=num, start_time = start_time, end_time = end_time)
+        else:
+            vector_db_result = self.processor.execute_operation(ModalityType.VECTOR, VectorHandlerOP.QUERY, queries, top_k=num, start_time = start_time, end_time = end_time)
         for item in vector_db_result:
             for (_id, modality_type) in item:
                 operation = TextHandlerOP.MGET
@@ -109,14 +128,15 @@ class DenseRetriever(BaseRetriever):
         
 if __name__ == "__main__":
     processor = MultiModalProcessor()
-    source_db = JSONFileDB("./db/flicker8k.json")
-    chroma_db = ChromaDB(collection_name="text_index", persist_directory="./db/flicker8k_index")
+    source_db = JSONFileDB("./db/test/test.json")
+    chroma_db = ChromaDB(collection_name="text_index", persist_directory="./db/test/test_index")
 
     processor.register_handler(ModalityType.TEXT, TextHandler(database=source_db))
     processor.register_handler(ModalityType.IMAGE, TextHandler(database=source_db))
+    # processor.register_handler(ModalityType.VECTOR, VectorHandler(database=chroma_db, embedding_func=embedding_functions.DefaultEmbeddingFunction()))
     processor.register_handler(ModalityType.VECTOR, VectorHandler(database=chroma_db, embedding_func=embedding_functions.DefaultEmbeddingFunction()))
     
-    dense_retriever = DenseRetriever(processor=processor)
+    dense_retriever = DenseRetriever(processor=processor, embedding_model = JinnaClip())
     retrieval_data = RetrievalData(items=[
         TextModalityData(
             content= "棕色狗狗",
@@ -130,3 +150,4 @@ if __name__ == "__main__":
     result = dense_retriever.search(retrieval_data, num=10)
     for r in result.items:
         print(r.metadata["summary"])
+        
