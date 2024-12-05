07f6695e9d149c30eb533d031d7f42f793ddb464
sys555 <sys555@outlook.com>
2024-12-05 15:16:57 +0800

    delete import

--- example/example.py
+++ example/example.py
@@ -1,15 +1,4 @@
-import base64
 import os
-from sentence_transformers import SentenceTransformer
-from aiki.database.chroma import ChromaDB
-from aiki.database.json_file import JSONFileDB
-from aiki.modal.retrieval_data import RetrievalData
-from aiki.multimodal.base import ModalityType, MultiModalProcessor
-from aiki.multimodal.text import TextHandler, TextModalityData
-from aiki.multimodal.vector import VectorHandler
-from aiki.retriever.retriever import DenseRetriever
-from bson import ObjectId
-
 from aiki.aiki import AIKI
 
 
